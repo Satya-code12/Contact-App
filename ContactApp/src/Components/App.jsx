5cc66b82ab2f3f52249146133fb5132cf73ce17e
Satyabrata Ojha <satyabrat637@gmail.com>
2024-08-15 16:03:32 +0530

    final commit

--- ContactApp/src/Components/App.jsx
+++ ContactApp/src/Components/App.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 import Header from "./Header";
-import AddContact from './AddContact';
-import ContactList from './ContactList';
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
 
-  const contacts =[
-    {
-      id: 1,
-      name: "Satyabrata",
-      email: "hello@gmail.com",
-    },
-    {
-      id: 2,
-      name: "HelloWorld",
-      email: "josep@gmail.com",
-    },
-  ]
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+  
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
-   <div className='ui container'>
-    <Header />
-    <AddContact />
-    <ContactList contacts= {contacts}/>
-   </div>
-  )
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
 }
 
 export default App;
--- ContactApp/src/Components/ContactList.jsx
+++ ContactApp/src/Components/ContactList.jsx
@@ -1,23 +1,22 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
-const ContactList =(props)=>{
+const ContactList = (props) => {
   console.log(props);
-  const renderContactList = props.contacts.map((items) =>{
-    return(
-      <div className="item">
-        <div className="content">
-          <div className="header">{items.name}</div>
-          <div>{items.email}</div>
-        </div>
-        <i className="trash alternate outline icon"></i>
-      </div>
-    )
-  })
-  return(
-    <div className="ui celled list">
-      {renderContactList}
-    </div>
-  );
-}
+
+  const deleteConactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHander={deleteConactHandler}
+        key={contact.id}
+      />
+    );
+  });
+  return <div className="ui celled list">{renderContactList}</div>;
+};
 
 export default ContactList;
